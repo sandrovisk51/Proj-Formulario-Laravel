7c357a3b7a74890f87406e391eb016a791d3512b
sandrovisk28 <sandrovisk28@outlook.com>
2023-11-23 20:53:47 -0300

    Criando migration Cliente

---
+++ database/migrations/2023_11_23_230116_create_tb_clientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 50);
+            $table->string('last_name', 100);
+            $table->string('email', 100)->index();
+            $table->string('cpf', 100)->unique()->index();
+            $table->date('birth_day');
+            $table->char('gender', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_clientes');
+    }
+};
