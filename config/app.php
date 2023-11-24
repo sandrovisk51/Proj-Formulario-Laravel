94a0a4d0fabbeef9266374ed995c27788dbf582c
sandrovisk28 <sandrovisk28@outlook.com>
2023-11-24 16:30:28 -0300

    Adicionando Lib validação pt-br

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        LaravelLegends\PtBrValidator\ValidatorProvider::class
     ])->toArray(),
 
     /*
